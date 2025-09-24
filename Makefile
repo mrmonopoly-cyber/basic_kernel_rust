1dc90b6fe52c04f02dacc8e1ef5f80a66617d989
Alberto Damo <alberto.damo@gmail.com>
2025-09-24 10:32:01 +0200

    feat(boot): added support for multiboot_v2 and updated the Makefile to choose

--- Makefile
+++ Makefile
@@ -1,23 +1,26 @@
 ISO ?= damos.iso
-BIN ?= isodir/boot/damos.bin
+ISOD ?= isodir
+BIN ?= $(ISOD)/boot/damos.bin
 TARGET ?= target/x86_64-unknown-none/release/basic_kernel
+FEATURES ?= 
+GRUB_FILE ?= --is-x86-multiboot2
 
 all: update run
 
 update:
 	rm -f $(ISO)
-	rm -f ./isodir/boot/damos.bin
+	rm -f $(BIN)
 
 run: $(ISO) 
 	qemu-system-x86_64 -cdrom $(ISO)
 
-$(ISO):  $(BIN)
-	$(shell  if ! grub-file --is-x86-multiboot $(BIN); then echo "damos.bin is not valid x86-multiboot format"; fi)
-	grub-mkrescue -o damos.iso ./isodir
+$(ISO): $(BIN)
+	# $(shell  if ! grub-file $(grub_file) $(bin); then echo "damos.bin is not valid x86-multiboot format"; fi)
+	grub-mkrescue -o damos.iso $(ISOD)
 
 
 $(BIN): 
-	cargo b --release
+	cargo b --release --features=$(FEATURES)
 	cp $(TARGET) $(BIN)
 
 clean:
--- src/boot/multiboot_v2/mod.rs
+++ src/boot/multiboot_v2/mod.rs
@@ -1,7 +1,41 @@
 pub mod log;
 
-#[allow(unused)]
-#[repr(align(16),C)]
-pub struct MultiBootV2{
+#[allow(unused,clippy::upper_case_acronyms)]
+#[repr(u32)]
+enum ArchTypes{
+    I386ProtectedMode =0,
+    MIPS = 4,
+    
 }
 
+#[allow(unused)]
+#[repr(align(64),C)]
+pub struct MultiBootV2<const N:usize>{
+    magic: u32,
+    arch: u32,
+    header_length: u32,
+    checksum: u32,
+    tags: [MultiBootV2Tag;N]
+}
+
+#[allow(unused)]
+#[repr(align(64),C)]
+pub struct MultiBootV2Tag{
+    t_type: u16,
+    t_flags: u16,
+    t_size: u32,
+}
+
+const END: MultiBootV2Tag = MultiBootV2Tag{t_type:0,t_flags:0,t_size:8};
+const MAGIC_V2: u32 = 0xE85250D6;
+const TAG_NUM: usize = 1;
+
+#[unsafe(link_section = ".multiboot")]
+#[used]
+pub static MULTIBOOT_HEADER: MultiBootV2<TAG_NUM>= MultiBootV2{
+    magic: MAGIC_V2,
+    arch: ArchTypes::I386ProtectedMode as u32,
+    header_length: core::mem::size_of::<MultiBootV2<TAG_NUM>>() as u32,
+    checksum: (MAGIC_V2 + ArchTypes::I386ProtectedMode as u32).wrapping_neg(),
+    tags : [END]
+};
